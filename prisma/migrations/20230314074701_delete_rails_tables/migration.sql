c6cc177cdb22c04ffe6a2e85407407ad9717a916
Kara Brightwell <kara@153.io>
2023-03-14 07:47:27 +0000

    delete rails-specific tables

---
+++ prisma/migrations/20230314074701_delete_rails_tables/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ar_internal_metadata` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `good_jobs` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `schema_migrations` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "ar_internal_metadata";
+
+-- DropTable
+DROP TABLE "good_jobs";
+
+-- DropTable
+DROP TABLE "schema_migrations";
