ce9cf24da3c2bcae8e1f83c31694598b97d4903c
Kara Brightwell <kara@153.io>
2023-03-21 15:57:01 +0000

    so sessions are easy huh

--- app/routes/log-in.tsx
+++ app/routes/log-in.tsx
@@ -2,15 +2,30 @@ import { Form } from '@remix-run/react'
 import { db } from '~/lib/db.server'
 import bcrypt from 'bcrypt'
 import { z } from 'zod'
-import type { ActionArgs } from '@remix-run/node'
+import type { ActionArgs, LoaderArgs } from '@remix-run/node'
 import { redirect } from '@remix-run/node'
+import { commitSession, getSession } from '~/session'
 
 const LoginFormSchema = z.object({
 	username: z.string(),
 	password: z.string(),
 })
 
+export async function loader({ request }: LoaderArgs) {
+	const session = await getSession(request.headers.get('cookie'))
+	if (session.get('userId')) {
+		return redirect('/', {
+			headers: {
+				'set-cookie': await commitSession(session),
+			},
+		})
+	}
+
+	return null
+}
+
 export async function action({ request }: ActionArgs) {
+	const session = await getSession(request.headers.get('cookie'))
 	const body = await request.formData()
 	const { username, password } = LoginFormSchema.parse(Object.fromEntries(body))
 
@@ -21,8 +36,13 @@ export async function action({ request }: ActionArgs) {
 	})
 
 	if (await bcrypt.compare(password, user.passwordDigest)) {
-		// TODO session
-		return redirect('/')
+		session.set('userId', user.id)
+
+		return redirect('/', {
+			headers: {
+				'set-cookie': await commitSession(session),
+			},
+		})
 	} else {
 		// TODO
 		throw new Error('nope')
