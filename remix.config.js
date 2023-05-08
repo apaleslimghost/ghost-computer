954455483b83c2947b690de20d72db8c9ad3f379
Kara Brightwell <kara@153.io>
2023-05-08 15:14:57 +0100

    use civet

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,17 @@
+const { withEsbuildOverride } = require('remix-esbuild-override')
+const civetPlugin = require('@danielx/civet/esbuild-plugin')
+const {
+	routeModuleExts,
+} = require('@remix-run/dev/dist/config/routesConvention')
+
+routeModuleExts.push('.civet')
+
+withEsbuildOverride((options) => {
+	options.plugins = [civetPlugin(), ...options.plugins]
+	return options
+})
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	ignoredRouteFiles: ['**/.*'],
-	// appDirectory: "app",
-	// assetsBuildDirectory: "public/build",
-	// serverBuildPath: "build/index.js",
-	// publicPath: "/build/",
 }
